8b5c5158499edc73f98d39f47be286887b71c7a6
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-10-11 21:02:50 +0200

    Create RoomsPage.jsx

---
+++ src/screens/RoomsPage.jsx
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from 'react';
+import {Row, Col, Container} from 'react-bootstrap';
+import Hero from "../components/Hero"
+import Banner from "../components/Banner"
+import {Link} from 'react-router-dom'
+import "./styles/RoomsPage.css"
+import RoomsPageOnlyRooms from "../components/RoomsPageOnlyRooms"
+
+
+const RoomsPage = () => {
+
+  
+    return (
+        <>
+        <Hero hero="roomsHero">
+          <Banner title="our rooms" >
+            <Link to="/" className="btn-primary">
+                return home
+            </Link>
+          </Banner>
+        </Hero>
+
+                <Container>
+                  <RoomsPageOnlyRooms />
+                </Container>
+        </>
+    )
+}
+
+export default RoomsPage
